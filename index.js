125e277ff76da7fdae17b490c0e7f495e5a68b1f
utkamag <92789705+utkamag@users.noreply.github.com>
2022-10-31 14:45:08 +0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from "mongoose";
 import router from "./Router.js";
+import cors from "cors"
 const PORT = process.env.PORT || 3000;
 const DB_URL = `mongodb+srv://utkamag:Hesoyam1973@cluster0.gfxjvcx.mongodb.net/?retryWrites=true&w=majority`
 
@@ -8,6 +9,9 @@ const app = express()
 
 app.use(express.json())
 app.use("/", router)
+app.use(cors({
+    origin: "*"
+}))
 
 async function startApp () {
     try {
