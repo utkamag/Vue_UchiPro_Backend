a54323775d44f53113f0a25e7c87c5b642f6eb9c
utkamag <92789705+utkamag@users.noreply.github.com>
2022-10-31 12:49:34 +0400

    update code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
 import router from "./router/Router.js";
-import Router from "./router/Router.js";
 const PORT = 3000;
 const DB_URL = `mongodb+srv://utkamag:Hesoyam1973@cluster0.gfxjvcx.mongodb.net/?retryWrites=true&w=majority`
 
@@ -9,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use("/", router)
-app.use("/tokens", Router)
+
 
 async function startApp () {
     try {
